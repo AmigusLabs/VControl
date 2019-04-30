4a915d1265cab09a8e064a2232402ef1f83513a1
CyberMODE <35894605+CyberMODE@users.noreply.github.com>
2019-04-30 18:00:26 +0200

    Update pantalla.cpp

--- src/pantalla.cpp
+++ src/pantalla.cpp
@@ -8,7 +8,7 @@ void pantallaBienvenida(U8G2 u8g2)
     u8g2.setFont(u8g2_font_logisoso16_tr);
     u8g2.drawStr(5, 38, "@migus Labs");
     u8g2.setFont(u8g2_font_helvR08_tr);
-    texto_version = "Power Module v4" + texto_version;
+    texto_version = "Power Module v" + texto_version;
     u8g2.drawStr(18, 54,  texto_version);
 
   } while (u8g2.nextPage());
